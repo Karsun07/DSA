ef946446b5636191434941588ed12c56b640ec2e
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-08-16 10:49:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.18%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
+    int fun(string &s,int i){
+        if(i==s.size()){
+            return stoi(s);
+        }
+        if(s[i]=='6'){
+            s[i]='9';
+            return stoi(s);
+        }
+         return fun(s,i+1);
+    }
     int maximum69Number (int num) {
-        vector<int>ans(5,0);
-        int divide=10000;
-        for(int i=0;i<5;i++){
-            ans[i]=num/divide;
-            num=num%divide;
-            divide=divide/10;
-        }
-        for(int i=0;i<5;i++){
-            if(ans[i]==6){
-                ans[i]=9;
-                break;
-            }
-        }
-        int sum=0;
-        divide=10000;
-        for(int i=0;i<5;i++){
-            sum+=ans[i]*divide;
-            divide/=10;
-
-        }
-        return sum;
+      string s=to_string(num);
+      return fun(s,0);
+      
 
     }
 };
