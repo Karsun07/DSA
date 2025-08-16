b83d42db9f3780ca4a2e5a7d81649693973b2815
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-08-16 18:24:37 +0530

    Time: 2 ms (36.86%), Space: 11 MB (29.97%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-void perm(vector<int> nums, vector<vector<int>>& ans, vector<int>& temp, vector<bool>& visited) {
-    if (temp.size() == nums.size()) { 
-        ans.push_back(temp);
-        return;
-    }
+    // void perm(vector<int>nums,int n,vector<int>visited,vector<int>temp,vector<vector<int>>&ans){
+    //     if(temp.size()==n){
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     for(int i=0;i<n;i++){
+    //         if(visited[i]==0){
+    //             visited[i]=1;
+    //             temp.push_back(arr[i]);
+    //             perm(,n,visited,temp,ans);
+    //             visited[i]=0;
+    //         }
+    //     }
+    // }
     
-    for (int i = 0; i < visited.size(); i++) {
-        if (!visited[i]) {  
-            visited[i] = true; 
-            temp.push_back(nums[i]);
-            perm(nums, ans, temp, visited);
-            temp.pop_back(); 
-            visited[i] = false; 
+    vector<vector<int>> ans;
+    void perm(vector<int>& nums, int index){
+        if(index == nums.size()-1){
+            ans.push_back(nums);
+        }
+        for(int i = index; i< nums.size(); i++){
+            swap(nums[i], nums[index]);
+            perm(nums, index + 1);
+            swap(nums[i], nums[index]);
         }
     }
-}
     vector<vector<int>> permute(vector<int>& nums) {
-    vector<vector<int>> ans;
-    vector<int> temp;
-    vector<bool> visited(nums.size(), false);
-    
-    perm(nums, ans, temp, visited);
-    return ans;
+        // int n=nums.size();
+        // vector<vector<int>>ans;
+        // vector<int>visited(n,0);
+        // vector<int>temp;
+        // perm(nums,n,visited,temp,ans);
+        perm(nums, 0);
+        return ans;
     }
 };
