7a05a2668a795fa86e4d1c2d34eeccd8759acc9e
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-08-18 13:54:49 +0530

    Time: 11 ms (1.63%), Space: 47.3 MB (5.12%) - LeetHub

--- 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
+++ 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    void fun(vector<int>deck,int i,queue<int>&q,vector<int>&ans){
+        if(q.empty()|| i==deck.size()) return;
+        ans[q.front()]=deck[i];
+        q.pop();
+        q.push(q.front());
+        q.pop();
+        fun(deck,i+1,q,ans);
+
+    }
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         queue<int> q;
         sort(deck.begin(), deck.end());
         int n = deck.size();
         vector<int> ans(n, 0);
-        for(int i = 0 ; i < n; i++){
+        for(int i=0;i<n;i++){
             q.push(i);
         }
-        for(int c : deck){
-            ans[q.front()] = c;
-            q.pop();
-            if(q.empty()){
-                return ans;
-            }
-            q.push(q.front());
-            q.pop();
-        }
+        fun(deck,0,q,ans);
         return ans;
     }
 };
