3509bd733eb7d388ce99471e9825845b4ed66bc0
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-08-23 22:55:17 +0530

    Time: 29 ms (76.74%), Space: 94.5 MB (15.33%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int n=nums.size();
-        if(n<2) return 0;
-        int maxdiff=INT_MIN;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            maxdiff=max(maxdiff,nums[i+1]-nums[i]);
+        int n = nums.size();
+        if (n < 2) return 0;
+
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
+
+        if (minVal == maxVal) return 0; // all elements same
+
+        // Bucket size and number of buckets
+        int bucketSize = max(1, (maxVal - minVal) / (n - 1));
+        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+
+        // Each bucket keeps track of min and max
+        vector<int> bucketMin(bucketCount, INT_MAX);
+        vector<int> bucketMax(bucketCount, INT_MIN);
+        vector<bool> used(bucketCount, false);
+
+        // Put numbers into buckets
+        for (int num : nums) {
+            int idx = (num - minVal) / bucketSize;
+            bucketMin[idx] = min(bucketMin[idx], num);
+            bucketMax[idx] = max(bucketMax[idx], num);
+            used[idx] = true;
         }
-        return maxdiff;
+
+        // Scan buckets to find max gap
+        int prevMax = minVal;
+        int maxGap = 0;
+        for (int i = 0; i < bucketCount; i++) {
+            if (!used[i]) continue;  // skip empty buckets
+            maxGap = max(maxGap, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
+        }
+
+        return maxGap;
     }
 };
