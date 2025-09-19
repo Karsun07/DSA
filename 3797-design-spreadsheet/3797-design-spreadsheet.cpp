b7989b98c29caf47488bb4c28e73885801df3d61
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-09-19 18:33:11 +0530

    Time: 59 ms (85.9%), Space: 127 MB (83.24%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.cpp
+++ 3797-design-spreadsheet/3797-design-spreadsheet.cpp
@@ -1,94 +1,27 @@
 class Spreadsheet {
 public:
-    int rows;
-    vector<vector<int>>m;
+    unordered_map<string, int> mp;
     Spreadsheet(int rows) {
-        m=vector<vector<int>>(rows,vector<int>(26,0));
-        
     }
     
     void setCell(string cell, int value) {
-        int c=cell[0]-'A';//A=>0 col
-        string num_str="";
-        for(int i=1;i<cell.size();i++){
-            num_str+=cell[i];
-        }
-        int r=stoi(num_str)-1;//1 row=>0
-        m[r][c]=value;//A1=>10
-
-        
-
-        
+        mp[cell] = value;
     }
     
     void resetCell(string cell) {
-        int c=cell[0]-'A';//A=>0 col
-        string num_str="";
-        for(int i=1;i<cell.size();i++){
-            num_str+=cell[i];
-        }
-        int r=stoi(num_str)-1;//1 row=>0
-        m[r][c]=0;
-
-    }
-    int fun(string cell){
-        int c=cell[0]-'A';//A=>0 col
-        string num_str="";
-        for(int i=1;i<cell.size();i++){
-            num_str+=cell[i];
-        }
-        int r=stoi(num_str)-1;//1 row=>0
-        return m[r][c];
-
-    }
-    bool isCell(string cell){
-        if(isalpha(cell[0])) return 1;
-        return 0;
+        mp[cell] = 0;
     }
     int getValue(string formula) {
-        int n=formula.size();
-        int ans=0;
-        int i=0;
-        while(i<n){
-            if(formula[i]=='='){
-                i++;
-                string temp="";
-                while(formula[i]!='+'){
-                    temp+=formula[i];
-                    i++;
-                }
-                if(isCell(temp))
-                    ans+=fun(temp);
-                else{
-                    int x=stoi(temp);
-                    ans+=x;
-                }
-
-            }
-            
-            
-            if(formula[i]=='+'){
-                i++;
-                string temp2="";
-                while(i<n){
-                    temp2+=formula[i];
-                    i++;
-                }
-                if(isCell(temp2))
-                   ans+=fun(temp2);
-                else{
-                    int y=stoi(temp2);
-                    ans+=y;
-                }
-
-            }
-
-        }
-        return ans;
-
-        
+        int m=formula.find('+');
+        int x, y;
+        if (formula[1]>='A') x=mp[formula.substr(1, m-1)];
+        else x=stoi(formula.substr(1, m-1));
+        if (formula[m+1]>='A') y=mp[formula.substr(m+1)];
+        else y=stoi(formula.substr(m+1));
+        return x+y;
     }
 };
+
 
 /**
  * Your Spreadsheet object will be instantiated and called as such:
