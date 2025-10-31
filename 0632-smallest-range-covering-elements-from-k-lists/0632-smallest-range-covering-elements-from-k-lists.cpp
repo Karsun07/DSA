1368bc449c9dcba7abdd474c058e740def928934
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-10-31 16:43:35 +0530

    Time: 68 ms (84.59%), Space: 36.7 MB (87.14%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        int n = nums.size();
-        vector<int> ans(2);
-        priority_queue<pair<int, pair<int, int>>, 
-                       vector<pair<int, pair<int, int>>>, 
-                       greater<pair<int, pair<int, int>>>> p;
-
-        int maximum = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            p.push({nums[i][0], {i, 0}});
-            maximum = max(maximum, nums[i][0]);
+        int k=nums.size();
+      
+        priority_queue< pair<int,pair<int,int>>,vector< pair<int,pair<int,int>>>,greater< pair<int,pair<int,int>>>>p;
+        int maxi=INT_MIN;
+        
+        for(int i=0;i<k;i++){
+            p.push({nums[i][0],{i,0}});
+            maxi=max(maxi,nums[i][0]);
         }
-
-        int minimum = p.top().first;
-        int r = maximum - minimum;
-        ans = {minimum, maximum};
-
-        while (p.size() == n) {
-            auto temp = p.top();
+        int mini=p.top().first;
+        int r=maxi-mini;
+        vector<int>ans(2);
+        ans={mini,maxi};
+        
+        pair<int,pair<int,int>>ele;
+        while(p.size()==k){
+            ele=p.top();
             p.pop();
-            int i = temp.second.first;
-            int j = temp.second.second;
-
-            if (j + 1 < nums[i].size()) {  
-                p.push({nums[i][j+1], {i, j+1}}); 
-                maximum = max(maximum, nums[i][j+1]);
-                minimum = p.top().first;
-
-                if (maximum - minimum < r) {
-                    r = maximum - minimum;
-                    ans[0] = minimum;
-                    ans[1] = maximum;
+            int i=ele.second.first;
+            int j=ele.second.second;
+            if(j+1<nums[i].size()){
+                p.push({nums[i][j+1],{i,j+1}});
+                mini=p.top().first;
+                maxi=max(maxi,nums[i][j+1]);
+                if(maxi-mini <r){
+                    r=maxi-mini;
+                    ans[0]=mini;
+                    ans[1]=maxi;
                 }
+
+                
             }
         }
         return ans;
