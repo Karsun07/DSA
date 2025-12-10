3734f666fd25f2277762ec7e91d505652b310917
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-12-11 00:28:04 +0530

    Time: 0 ms (100%), Space: 8 MB (36.86%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
+    bool isvow(char c){
+        char x=tolower(c);
+        if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u') return 1;
+        return 0;
+    }
     bool isValid(string word) {
         int n=word.size();
         if(n<3) return 0;
         bool vow=0,cons=0;
         for(int i=0;i<n;i++){
-            if(word[i]=='@'||word[i]=='#'||word[i]=='$') return 0;
-            else if(word[i]=='1'||word[i]=='2'||word[i]=='3'||word[i]=='4'||word[i]=='5'||word[i]=='6'||word[i]=='7'||word[i]=='8'||word[i]=='9') continue;
-           else if(word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'||word[i]=='A'||word[i]=='E'||word[i]=='I'||word[i]=='O'||word[i]=='U') vow=1;
-           else cons=1;
-            
-
+            if(!isalpha(word[i]) && !isdigit(word[i])) return 0;
+            if(isalpha(word[i])){
+                if(isvow(word[i])) vow=1;
+                else cons=1;
+            }
+           
         }
-        if(vow&&cons) return 1;
+        if(vow && cons) return 1;
         return 0;
+        
     }
 };
