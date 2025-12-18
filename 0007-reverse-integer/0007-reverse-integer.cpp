54a2566f3bcbb0e4ef498b268484cf2b3bd59537
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2025-12-18 23:22:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long z = llabs((long long)x);  
-        string y = to_string(z);
-        std::reverse(y.begin(), y.end());
-
-        int n = y.size();
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
-            arr[i] = y[i] - '0';
+        int ans = 0; 
+        while (x != 0) {
+            int digit = x % 10; 
+            
+            
+            if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)) {
+                return 0; 
+            }
+            
+            ans = ans * 10 + digit; 
+            x = x / 10; 
         }
-
-        long long ans = 0;                
-        int j = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            ans = ans + arr[i] * (long long)pow(10, j);
-            j++;
-        }
-
-        if (x < 0) ans = -ans;
-
-        if (ans > INT_MAX || ans < INT_MIN) return 0;  // FIX 3
-        return (int)ans;
+        return ans; 
     }
 };
