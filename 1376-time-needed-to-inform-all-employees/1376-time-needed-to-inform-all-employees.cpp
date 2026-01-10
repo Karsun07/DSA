5ab081c942bcf75c581c1e96eba0b3a475cfaff1
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-01-10 12:17:19 +0530

    Time: 99 ms (86.78%), Space: 126.6 MB (61.51%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    
+    int dfs(int node,vector<vector<int>>&adj,vector<bool>&visited,vector<int>&informTime){
+        int ans=0;
+        visited[node]=1;
+        for(int j=0;j<adj[node].size();j++){
+            if(!visited[adj[node][j]]){
+                ans=max(ans,informTime[node]+dfs(adj[node][j],adj,visited,informTime));
+            }
+        }
+        return ans;
+    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         int V=n;
         if(V==1) return 0;
@@ -8,20 +19,8 @@ public:
             int u=manager[i],v=i;
             if(u!=-1) adj[u].push_back(v);
         }
-        int totalTime=0;
-        queue<pair<int,int>>q;
-        q.push({headID,0});
-        while(!q.empty()){
-            int man=q.front().first;
-            int time=q.front().second;
-            q.pop();
-            totalTime=max(totalTime,time);
-            for(int j=0;j<adj[man].size();j++){
-                int emp=adj[man][j];
-                q.push({emp,time+informTime[man]});
-            }
-        }
-        return totalTime;
+        vector<bool>visited(V,0);
+        return dfs(headID,adj,visited,informTime);
 
     }
 };
