6a61cb49a2370bf1a8f80bdd8f3933936e326777
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-01-22 17:57:20 +0530

    Time: 5 ms (37.57%), Space: 18.5 MB (34.04%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto &f : flights) {
-            adj[f[0]].push_back({f[1], f[2]});
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto flight : flights){
+            adj[flight[0]].push_back({flight[1], flight[2]});
         }
-
-        
-        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<pair<int, pair<int,int>>>
-        > pq;
-
-        pq.push({0, {src, 0}});
-        dist[src][0] = 0;
-
-        while (!pq.empty()) {
-            auto cur = pq.top(); pq.pop();
-            int cost = cur.first;
-            int node = cur.second.first;
-            int stops = cur.second.second;
-
-            if (node == dst) return cost;
-            if (stops == k + 1) continue;
-
-            for (auto &e : adj[node]) {
-                int next = e.first;
-                int wt = e.second;
-                if (cost + wt < dist[next][stops + 1]) {
-                    dist[next][stops + 1] = cost + wt;
-                    pq.push({cost + wt, {next, stops + 1}});
+       
+        queue<pair<int, int>> q;
+        q.push({src, 0});
+        vector<int> minCost(n, INT_MAX);
+        int stops = 0;
+        while(!q.empty() && stops <= k){
+            int size = q.size();
+            while (size--) {
+                auto [currNode, cost] = q.front();
+                q.pop();
+                for (auto& [neighbour, price] : adj[currNode]) {
+                    if (price + cost < minCost[neighbour]){
+                        minCost[neighbour] = price + cost;
+                        q.push({neighbour, minCost[neighbour]});
+                    }
                 }
             }
+            stops++;
         }
-
-        return -1;
+        if(minCost[dst] == INT_MAX)
+            return -1;
+        return minCost[dst];
     }
 };
