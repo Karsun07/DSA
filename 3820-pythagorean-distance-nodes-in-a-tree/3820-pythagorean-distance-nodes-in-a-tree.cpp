c3330553c9809d61f63f24bad9893d9e53dbcd5f
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-01-25 11:39:39 +0530

    Time: 369 ms (86.33%), Space: 378.7 MB (87.73%) - LeetHub

---
+++ 3820-pythagorean-distance-nodes-in-a-tree/3820-pythagorean-distance-nodes-in-a-tree.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int>bfs(int start,vector<vector<int>>&adj){
+        vector<int>dist(adj.size(),0);
+        vector<bool>visited(adj.size(),0);
+        visited[start]=1;
+        queue<int>q;
+        q.push(start);
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                int node=q.front();
+                q.pop();
+                for(int j=0;j<adj[node].size();j++){
+                    int nb=adj[node][j];
+                    if(!visited[nb]){
+                        visited[nb]=1;
+                        dist[nb]=dist[node]+1;
+                        q.push(nb);
+                    }
+                }
+            }
+        }
+        return dist;
+
+
+    }
+    int specialNodes(int n, vector<vector<int>>& edges, int x, int y, int z) {
+       vector<vector<int>>adj(n);
+       for(int i=0;i<edges.size();i++){
+        int u=edges[i][0],v=edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+       }
+       vector<int>arr1=bfs(x,adj);
+       vector<int>arr2=bfs(y,adj);
+       vector<int>arr3=bfs(z,adj);
+       int ans=0;
+       for(int i=0;i<n;i++){
+        long long a=arr1[i],b=arr2[i],c=arr3[i];
+        long long maxi=max({a,b,c});
+        if(a*a+b*b+c*c==2*maxi*maxi) ans++;
+       }
+       return ans;
+    }
+};
