45eeb3142457279a05d0228ca8cfd1798b40873f
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-01-28 16:49:29 +0530

    Time: 10 ms (18.05%), Space: 18.8 MB (23.98%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -6,26 +6,31 @@ public:
             adj[flight[0]].push_back({flight[1], flight[2]});
         }
        
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        vector<int> minCost(n, INT_MAX);
-        int stops = 0;
-        while(!q.empty() && stops <= k){
-            int size = q.size();
-            while (size--) {
-                auto [currNode, cost] = q.front();
-                q.pop();
-                for (auto& [neighbour, price] : adj[currNode]) {
-                    if (price + cost < minCost[neighbour]){
-                        minCost[neighbour] = price + cost;
-                        q.push({neighbour, minCost[neighbour]});
-                    }
+     
+        vector<vector<int>>dist(n,vector<int>(k+2,INT_MAX));
+        // k+1 nodes including dst=>size=>k+2
+        // {cost,{stops,node}}
+        
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        dist[src][0]=0;
+        pq.push({0,{0,src}});
+        while(!pq.empty()){
+            auto x=pq.top();
+            pq.pop();
+            int cost=x.first;
+            int stops=x.second.first;
+            int node=x.second.second;
+            if(node==dst) return cost;
+            if(stops>k) continue;
+            for(auto &it:adj[node]){
+                int nb=it.first;
+                int price=it.second;
+                if(cost+price<dist[nb][stops+1]){
+                    dist[nb][stops+1]=cost+price;
+                    pq.push({dist[nb][stops+1],{stops+1,nb}});
                 }
             }
-            stops++;
         }
-        if(minCost[dst] == INT_MAX)
-            return -1;
-        return minCost[dst];
+        return -1;
     }
 };
