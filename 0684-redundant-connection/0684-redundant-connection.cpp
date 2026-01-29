840956e2202ab878b28dc8b06abe1be6e57d3cc8
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-01-30 00:05:41 +0530

    Time: 0 ms (100%), Space: 14 MB (26.63%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool dfs(int node, int parent, vector<int> adj[], vector<bool>& vis) {
+        vis[node] = true;
+        for (auto adjnode : adj[node]) {
+            if (!vis[adjnode]) {
+                if (dfs(adjnode, node, adj, vis))
+                    return true;
+            } else if (adjnode != parent)
+                return true;
+        }
+        return false;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> adj[n + 1];
+        for (int i = 0; i < n; i++) {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+            vector<bool> vis(n + 1, false);
+            if (dfs(edges[i][0], -1, adj, vis)) {
+                return edges[i];
+            }
+        }
+        return {};
+    }
+};
