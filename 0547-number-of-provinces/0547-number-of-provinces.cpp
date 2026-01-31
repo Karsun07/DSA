ed018f898eaa6decb5f0b35800d8718083758c24
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-01-31 11:33:08 +0530

    Time: 1 ms (48.6%), Space: 19.7 MB (54.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>>&adj,vector<bool>&visited){
-        visited[node]=1;
-        for(int j=0;j<adj[node].size();j++){
-            int nb=adj[node][j];
-            if(!visited[nb]){
-                dfs(nb,adj,visited);
-            }
+    int findParent(int u,vector<int>&parent){
+        if(u==parent[u]) return u;
+        return parent[u]=findParent(parent[u],parent);
+    }
+    void unionbyrank(int u,int v,vector<int>&parent,vector<int>&rank){
+        int pu=findParent(u,parent);
+        int pv=findParent(v,parent);
+        if(pu==pv) return;
+        if(rank[pu]>rank[pv]) parent[pv]=pu;
+        else if(rank[pu]<rank[pv]) parent[pu]=pv;
+        else{
+            parent[pv]=pu;
+            rank[pu]++;
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<vector<int>>adj(n);
+        vector<int>rank(n,0);
+        vector<int>parent(n);
+        for(int i=0;i<n;i++) parent[i]=i;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j] && i!=j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    unionbyrank(i,j,parent,rank);
                 }
             }
         }
-        vector<bool>visited(n,0);
-        int ans=0;
+        unordered_set<int>set;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,adj,visited);
-                ans++;
-            }
+            set.insert(findParent(i,parent));
         }
-        return ans;
+        return set.size();
+       
     }
 };
