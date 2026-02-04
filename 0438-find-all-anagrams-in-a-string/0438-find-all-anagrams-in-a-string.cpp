32dda15e4ea73be2c118875e02f0186249f740ba
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-02-04 10:01:17 +0530

    Time: 23 ms (22.97%), Space: 15.7 MB (14.01%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-      int n=s.size();
-      int win=p.size();
-      if(win>n) return {};
-      unordered_map<char,int>mp;
-      for(int i=0;i<win;i++){
-        mp[p[i]]++;
-      }
-      unordered_map<char,int>temp;
-      int l=0,r=0;
-      vector<int>ans;
-      while(r<n){
-        temp[s[r]]++;
-        r++;
-        if(r-l==win){
-            bool flag=1;
-            for(int i=0;i<win;i++){
-                if(temp[p[i]]==mp[p[i]]) continue;
-                else{
-                    flag=0;
-                    break;
-                } 
-            }
-            if(flag) ans.push_back(l);
-            temp[s[l]]--;
-            l++;
+        int n = s.size();
+        int win = p.size();
+
+        if (win > n) return {};
+
+        unordered_map<char,int> mp;
+        for (int i = 0; i < win; i++) {
+            mp[p[i]]++;
         }
 
-      }
-      return ans;
-      
+        unordered_map<char,int> temp;
+        int l = 0, r = 0;
+        vector<int> ans;
+
+        while (r < n) {
+            temp[s[r]]++;
+            r++;
+
+            if (r - l == win) {       
+                if (temp == mp) ans.push_back(l);
+
+                temp[s[l]]--;          
+                if (temp[s[l]] == 0) temp.erase(s[l]);
+                l++;
+            }
+        }
+
+        return ans;
     }
 };
