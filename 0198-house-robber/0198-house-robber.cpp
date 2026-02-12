cfc2e999185427e8d8f4ef087a1952730199a264
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-02-12 09:28:12 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.69%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+       if(n==1) return nums[0];
+       if(n==2) return max(nums[0],nums[1]);
+       if(nums[0]>nums[1]) nums[1]=nums[0];
+       int ans=INT_MIN;
+       for(int i=2;i<n;i++){
+          nums[i]=max(nums[i-2]+nums[i],nums[i-1]);
+          ans=max(ans,nums[i]);
+       }
+       return ans;
+       
+
+    }
+};
