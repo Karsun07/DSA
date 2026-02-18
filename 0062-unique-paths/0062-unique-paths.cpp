9092a35313a3b57535f46358121be17728195fde
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-02-18 11:39:11 +0530

    Time: 1 ms (25.41%), Space: 9.5 MB (28.9%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
+    bool valid(int i, int j, int m, int n){
+        return i < m && j < n;
+    }
+
+    int fun(int i, int j, int m, int n, vector<vector<int>>& dp){
+        if(i == m-1 && j == n-1) return 1;
+
+        if(!valid(i, j, m, n)) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int d = fun(i+1, j, m, n, dp);
+        int r = fun(i, j+1, m, n, dp);
+
+        return dp[i][j] = d + r;
+    }
+
     int uniquePaths(int m, int n) {
-        int N = m + n - 2;    
-        int r = min(m-1, n-1); 
-        long long res = 1;     
-       
-        for (int i = 1; i <= r; i++) {
-            res = res * (N - r + i) / i;
-        }
-        
-        return (int)res;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return fun(0, 0, m, n, dp);
     }
 };
