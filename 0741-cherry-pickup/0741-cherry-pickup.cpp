24e7b15b8a796f3460484d76277a016944c3b15a
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-03-14 22:23:23 +0530

    Time: 59 ms (30.45%), Space: 28.3 MB (51.08%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,51 +1,48 @@
 class Solution {
 public:
     int n;
+    vector<vector<vector<int>>> memo;
 
-    int fun(int r1,int c1,int r2,int c2,vector<vector<int>>&grid,
-            vector<vector<vector<vector<int>>>>&dp){
+    int solve(vector<vector<int>>& grid, int r1, int c1, int r2) {
 
-        if(r1>=n||c1>=n||r2>=n||c2>=n||grid[r1][c1]==-1||grid[r2][c2]==-1)
-            return INT_MIN;
+        int c2 = r1 + c1 - r2;
+
+        if(r1>=n || c1>=n || r2>=n || c2>=n ||
+           grid[r1][c1]==-1 || grid[r2][c2]==-1)
+            return -1e9;
 
         if(r1==n-1 && c1==n-1)
             return grid[r1][c1];
 
-        if(dp[r1][c1][r2][c2] != -1)
-            return dp[r1][c1][r2][c2];
+        if(memo[r1][c1][r2] != -1)
+            return memo[r1][c1][r2];
 
-        int cherry=0;
+        int cherries = grid[r1][c1];
 
-        if(r1==r2 && c1==c2)
-            cherry+=grid[r1][c1];
-        else
-            cherry+=grid[r1][c1]+grid[r2][c2];
+        if(r1!=r2)
+            cherries += grid[r2][c2];
 
-        int d1=fun(r1+1,c1,r2+1,c2,grid,dp);
-        int d2=fun(r1,c1+1,r2,c2+1,grid,dp);
-        int d3=fun(r1+1,c1,r2,c2+1,grid,dp);
-        int d4=fun(r1,c1+1,r2+1,c2,grid,dp);
+        int d1 = solve(grid,r1+1,c1,r2+1);
+        int d2 = solve(grid,r1,c1+1,r2);
+        int d3 = solve(grid,r1+1,c1,r2);
+        int d4 = solve(grid,r1,c1+1,r2+1);
 
         int best = max({d1,d2,d3,d4});
 
-        if(best == INT_MIN)
-            return dp[r1][c1][r2][c2] = INT_MIN;
+        cherries += best;
 
-        cherry += best;
-
-        return dp[r1][c1][r2][c2] = cherry;
+        return memo[r1][c1][r2] = cherries;
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
 
         n = grid.size();
 
-        vector<vector<vector<vector<int>>>> dp(
-            n, vector<vector<vector<int>>>(n,
-            vector<vector<int>>(n,
-            vector<int>(n,-1))));
+        memo = vector<vector<vector<int>>>(
+            n, vector<vector<int>>(n, vector<int>(n,-1))
+        );
 
-        int ans = fun(0,0,0,0,grid,dp);
+        int ans = solve(grid,0,0,0);
 
         return max(0,ans);
     }
