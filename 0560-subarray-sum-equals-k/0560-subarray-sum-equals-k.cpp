cef70d279da7d022a7adaa095dbf172be63be7e2
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-03-20 11:26:22 +0530

    Time: 43 ms (58.45%), Space: 45.5 MB (38.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int>freq;
-        freq[0]++; // if k=2,aur hame agar 2 mil gaya iska matlab wo index 0 se shuru hua ,find(sum-k) to 0 milega kyuki ye hame pehle nahi mila ,lekin isko lena chahiye kyuki ye sum req hai isliye ye edge case hai 
-        int count = 0, sum = 0;
-        
-        for (int i=0;i<nums.size();i++) {
-            sum += nums[i];
-            if (freq.find(sum - k) != freq.end()) {
-                count += freq[sum - k];
-            }
-            freq[sum]++;
+       int n=nums.size();
+       int sum=0,count=0;
+       unordered_map<int,int>mp;
+       mp[0]=1;
+       for(int i=0;i<n;i++){
+        sum+=nums[i];
+        if(mp.count(sum-k)){
+            count+=mp[sum-k];
         }
-
-        return count;
+        mp[sum]++;
+       }
+       return count;
     }
 };
