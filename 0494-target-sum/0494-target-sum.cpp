87763e94dc119f513e2b75796f90dcd17221683d
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-03-24 21:53:28 +0530

    Time: 4 ms (77.1%), Space: 15.5 MB (48.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     int n;
-    int fun(int i, int target, vector<int>& nums, vector<vector<int>>& dp, int offset) {
 
-    if (target < 0 || target >= dp[0].size()) return 0;
+    int fun(int i, int target, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == n) {
+            return target == 0;
+        }
 
-    if (i == n) {
-        return target == offset;
+        if (dp[i][target] != -1) return dp[i][target];
+
+     
+        if (nums[i] == 0) {
+            return dp[i][target] = 2 * fun(i + 1, target, nums, dp);
+        }
+
+        int take = 0;
+        if (target >= nums[i]) {
+            take = fun(i + 1, target - nums[i], nums, dp);
+        }
+
+        int nottake = fun(i + 1, target, nums, dp);
+
+        return dp[i][target] = take + nottake;
     }
 
-    if (dp[i][target] != -1) return dp[i][target];
-
-    int plus = fun(i + 1, target + nums[i], nums, dp, offset);
-    int minus = fun(i + 1, target - nums[i], nums, dp, offset);
-
-    return dp[i][target] = plus + minus;
-}
     int findTargetSumWays(vector<int>& nums, int target) {
         n = nums.size();
 
         int totalsum = 0;
         for (int x : nums) totalsum += x;
-        if (target > totalsum || target < -totalsum) return 0;
-        
-        vector<vector<int>> dp(n, vector<int>(2 * totalsum + 1, -1));
 
-        return fun(0, target + totalsum, nums, dp, totalsum);
+       
+        if ((totalsum - target) < 0 || (totalsum - target) % 2 != 0)
+            return 0;
+
+        target = (totalsum - target) / 2;
+
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+
+        return fun(0, target, nums, dp);
     }
 };
