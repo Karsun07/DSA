de2bbd8878c48dfb4ba7d5deee0112294b10955b
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-03-25 23:12:39 +0530

    Time: 33 ms (42.64%), Space: 36.3 MB (62.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int n;
-    int fun(int i,int amt,vector<int>&coins,vector<vector<int>>&dp){
-        if(amt==0) return 1;
-        if(i==n-1){
-            if(amt%coins[i]==0){
-                return 1;
-            }
-            else return 0;
-        }
-        if(dp[i][amt]!=-1) return dp[i][amt];
-        int nottake=fun(i+1,amt,coins,dp);
-        int take=0;
-        if(amt>=coins[i]){
-            take=fun(i,amt-coins[i],coins,dp);
-        }
-        return dp[i][amt]=take+nottake;
-    }
     int change(int amount, vector<int>& coins) {
         n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return fun(0,amount,coins,dp);
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        for(int amt=0;amt<=amount;amt++){
+            dp[n-1][amt]=amt%coins[n-1]==0?1:0;
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int amt=1;amt<=amount;amt++){
+                long long nottake=dp[i+1][amt];
+                long long take=0;
+                if(amt>=coins[i]){
+                   take=dp[i][amt-coins[i]];
+                }
+                dp[i][amt]=take+nottake;
+            }
+        }
+
+        return dp[0][amount];
     }
 };
