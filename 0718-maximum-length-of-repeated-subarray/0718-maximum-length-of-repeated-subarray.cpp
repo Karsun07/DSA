1a52d69478587b1e82f7c181d17a60a7bf9ef42c
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-03-29 19:01:24 +0530

    Time: 87 ms (56.97%), Space: 89.5 MB (11.78%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int n1,n2;
-    int ans=0;
-    int fun(int i,int j,vector<int>&nums1,vector<int>&nums2,vector<vector<int>>&dp){
-        if(i>=n1 || j>=n2) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int curr=0;
-        if(nums1[i]==nums2[j]){
-            curr=1+fun(i+1,j+1,nums1,nums2,dp);
-            ans=max(ans,curr);
-        }
-        fun(i+1,j,nums1,nums2,dp);
-        fun(i,j+1,nums1,nums2,dp);
-        return dp[i][j]=curr;
-
-    }
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         n1=nums1.size(),n2=nums2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        fun(0,0,nums1,nums2,dp);
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        int ans=0;
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                if(nums1[i]==nums2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                    ans=max(ans,dp[i][j]);
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
         return ans;
     }
 };
