375fa3c64ddad9efac9d2beaed7af56bba156983
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-03-30 10:34:21 +0530

    Time: 11 ms (67.51%), Space: 16.8 MB (56.63%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int n1, n2;
-
-    int lcs(int i, int j, string &word1, string &word2, vector<vector<int>>& dp) {
-        if (i >= n1 || j >= n2) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (word1[i] == word2[j]) {
-            return dp[i][j] = 1 + lcs(i + 1, j + 1, word1, word2, dp);
-        }
-
-        return dp[i][j] = max(
-            lcs(i + 1, j, word1, word2, dp),
-            lcs(i, j + 1, word1, word2, dp)
-        );
-    }
-
     int minDistance(string word1, string word2) {
         n1 = word1.size();   
         n2 = word2.size();   
 
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1,0));
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                if(word1[i]==word2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return n1+n2-2*dp[0][0];
 
-        int common = lcs(0, 0, word1, word2, dp);
-
-        return (n1 - common) + (n2 - common);
+       
     }
 };
