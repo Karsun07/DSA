dab8c90c21d393196f774d7308f9aa808b22f987
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-04-01 22:34:43 +0530

    Time: 39 ms (22.38%), Space: 17.4 MB (75.4%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    int ns, nt;
+    int n, m;
     long long dp[1001][1001];
-
-    long long fun(int i, int j, string &s, string &t) {
-        if (j == nt) return 1;
-        if (i == ns) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        long long count = 0;
-
-        if (s[i] == t[j]) {
-            count += fun(i + 1, j + 1, s, t);
+    int rec(string &s, string &t, int i, int j){
+        if(j==m){
+            return 1;
         }
-
-        count += fun(i + 1, j, s, t);
-
-        return dp[i][j] = count;
+        if(i==n){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        long long ans = 0;
+        if(s[i]==t[j]){
+            ans+=rec(s, t, i+1, j+1);
+        }
+        ans+=rec(s, t, i+1, j);
+        return dp[i][j] = ans;
     }
 
     int numDistinct(string s, string t) {
-        ns = s.size();
-        nt = t.size();
-
-        for (int i = 0; i <= ns; i++) {
-            for (int j = 0; j <= nt; j++) {
+        n = s.size(), m = t.size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 dp[i][j] = -1;
             }
         }
-
-        return fun(0, 0, s, t);
+        return rec(s, t, 0, 0);
     }
 };
