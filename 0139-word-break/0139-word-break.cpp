36787a8c470430ad7b7ffcb61a469c33664c370c
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-04-07 12:38:49 +0530

    Time: 15 ms (28.41%), Space: 17.7 MB (19.54%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int ns,nd;
     unordered_set<string>st;
-    bool fun(int i,string s,vector<int>&dp){
-        if(i>=ns) return true;
-        if(dp[i]!=-1) return dp[i];
-        for(int idx=i;idx<ns;idx++){
-            string x=s.substr(i,idx-i+1);
-            if(st.count(x)){
-              if(fun(idx+1,s,dp)) return dp[i]=true;
-            }
-        }
-        return dp[i]=false;
-    
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         ns=s.size(),nd=wordDict.size();
         for(int i=0;i<nd;i++){
             st.insert(wordDict[i]);
         }
-        vector<int>dp(ns,-1);
-        return fun(0,s,dp);
-
+        vector<int>dp(ns+1,0);
+        dp[ns]=1;
+        for(int i=ns-1;i>=0;i--){
+           for(int idx=i;idx<ns;idx++){
+                string x=s.substr(i,idx-i+1);
+                if(st.count(x)){
+                    if(dp[idx+1]) dp[i]=true;
+                }
+            }
+           
+        }
+        return dp[0];
     }
 };
