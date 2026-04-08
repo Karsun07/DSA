12979cc8662ee3b0c88b5e7d9a75c410e9f97ebc
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-04-08 19:56:12 +0530

    Time: 0 ms (100%), Space: 9.7 MB (39.99%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int n1,n2,n3;
-    bool fun(int i,int j,int k,string &s1,string &s2,string &s3,vector<vector<vector<int>>>&dp){
+    bool fun(int i,int j,string &s1,string &s2,string &s3,vector<vector<int>>&dp){
+        int k=i+j;
         if(i==n1 && j==n2 && k==n3) return true;
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
+        if(dp[i][j]!=-1) return dp[i][j];
         bool res=false;
         if(i<n1 && s1[i]==s3[k]){
-            if(fun(i+1,j,k+1,s1,s2,s3,dp)) res=true;
+            if(fun(i+1,j,s1,s2,s3,dp)) res=true;
         }
         if(j<n2 &&  s2[j]==s3[k]){
-            if(fun(i,j+1,k+1,s1,s2,s3,dp)) res=true;
+            if(fun(i,j+1,s1,s2,s3,dp)) res=true;
         }
-        return dp[i][j][k]=res;
+        return dp[i][j]=res;
         
     }
     bool isInterleave(string s1, string s2, string s3) {
         n1=s1.size(),n2=s2.size(),n3=s3.size();
         if(n1+n2!=n3) return false;
-        vector<vector<vector<int>>>dp(n1+1,vector<vector<int>>(n2+1,vector<int>(n3+1,-1)));
-        return fun(0,0,0,s1,s2,s3,dp);
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
+        return fun(0,0,s1,s2,s3,dp);
 
     }
 };
