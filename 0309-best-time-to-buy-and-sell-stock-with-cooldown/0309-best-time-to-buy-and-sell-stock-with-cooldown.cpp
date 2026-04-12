a10a80a43c34bc21b4eae9bb2af0e1fd733645b8
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-04-12 21:32:57 +0530

    Time: 4 ms (20.44%), Space: 16.8 MB (7.41%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int n;
-    int fun(int i,bool buy,bool cooldown,vector<int>&prices,vector<vector<vector<int>>>&dp){
-        if(i==n) return 0;
-        if(dp[i][buy][cooldown]!=-1) return dp[i][buy][cooldown];
-        int profit;
-        if(cooldown){
-            profit=fun(i+1,1,0,prices,dp);
-        }
-        else if(buy){
-            profit=max(-prices[i]+fun(i+1,false,cooldown,prices,dp),fun(i+1,true,cooldown,prices,dp));
-        }
-        else if(!buy){
-            profit=max(prices[i]+fun(i+1,true,true,prices,dp),fun(i+1,false,cooldown,prices,dp));
-        }
-        return dp[i][buy][cooldown]=profit;
-    }
     int maxProfit(vector<int>& prices) {
         n=prices.size();  
-      vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
-      return fun(0,true,0,prices,dp);
-    }
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(2,0)));
+ 
+         for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cooldown=1;cooldown>=0;cooldown--){
+                    int profit;
+                    if(cooldown){
+                        profit=dp[i+1][1][0];
+                    }
+                    else if(buy){
+                    profit=max(-prices[i]+dp[i+1][0][cooldown],dp[i+1][1][cooldown]);
+                    }
+                    else if(!buy){
+                        profit=max(prices[i]+dp[i+1][1][1],dp[i+1][0][cooldown]);
+                    }
+                    dp[i][buy][cooldown]=profit;
+                }
+
+                }
+            }
+            return dp[0][1][0];
+
+         }
+        
+    
 };
