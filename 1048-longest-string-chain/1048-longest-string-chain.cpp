7a55c9eea76b0246a82586ad7d27adc2ee4ea497
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-05-20 23:07:44 +0530

    Time: 62 ms (46.71%), Space: 22.1 MB (50.09%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,43 +1,58 @@
 class Solution {
 public:
     int dp[1001][1001];
-    
+
     static bool cmp(string &a, string &b) {
         return a.size() < b.size();
     }
 
-    bool match(string &w1, string &w2) {
-        if(w2.size() != w1.size() + 1) return false;
-        int i = 0, j = 0, skip = 0;
-        while(i < w1.size() && j < w2.size()) {
-            if(w1[i] != w2[j]) {
-                skip++;
+    bool match(string &a, string &b) {
+        if (b.size() != a.size() + 1)
+            return false;
+
+        int i = 0, j = 0;
+        bool skipped = false;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] == b[j]) {
+                i++;
                 j++;
             } else {
-                i++;
+                if (skipped)
+                    return false;
+
+                skipped = true;
                 j++;
             }
         }
-        return skip <= 1 && i == w1.size();
-    }
 
-    int fun(int curr, int prev, vector<string>& words) {
-        if(curr >= words.size()) return 0;
-
-        if(dp[curr][prev + 1] != -1) return dp[curr][prev + 1];
-
-        int take = 0;
-        if(prev == -1 || match(words[prev], words[curr])) {
-            take = 1 + fun(curr + 1, curr, words);
-        }
-        int nottake = fun(curr + 1, prev, words);
-
-        return dp[curr][prev + 1] = max(take, nottake);
+        return true;
     }
 
     int longestStrChain(vector<string>& words) {
+
         sort(words.begin(), words.end(), cmp);
-        memset(dp, -1, sizeof(dp));
-        return fun(0, -1, words);
+
+        int n = words.size();
+        for (int j = 0; j <= n; j++) {
+            dp[n][j] = 0;
+        }
+        for (int curr = n - 1; curr >= 0; curr--) {
+
+            for (int prev = curr - 1; prev >= -1; prev--) {
+
+                int take = 0;
+
+                if (prev == -1 || match(words[prev], words[curr])) {
+                    take = 1 + dp[curr + 1][curr + 1];
+                }
+
+                int nottake = dp[curr + 1][prev + 1];
+
+                dp[curr][prev + 1] = max(take, nottake);
+            }
+        }
+
+        return dp[0][0];
     }
 };
