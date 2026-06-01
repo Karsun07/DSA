84f0c38c6797ca45c185bb091041040f6fa7c284
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-06-01 13:46:10 +0530

    Time: 4 ms (69.93%), Space: 12.4 MB (58.96%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int n;
-    int fun(int i, int k, vector<int>& arr,vector<int>&dp) {
-        if (i == n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ans = 0;
-        int maxi = 0;
-        for (int j = i; j < min(n, i + k); j++) {
-            maxi = max(maxi, arr[j]);
-            int len = j - i + 1;
-            ans = max(ans,maxi * len + fun(j + 1, k, arr,dp));
-        }
-
-        return dp[i]=ans;
-    }
-
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         n = arr.size();
-        vector<int>dp(n,-1);
-        return fun(0, k, arr,dp);
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int ans = 0;
+            int maxi = 0;
+            for (int j = i; j < min(n, i + k); j++) {
+                maxi = max(maxi, arr[j]);
+                int len = j - i + 1;
+                ans = max(ans,maxi * len + dp[j + 1]);
+            }
+
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
