b2ba12892cde3ec034a4915a053dffc39b52f45c
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-06-16 12:34:16 +0530

    Time: 2 ms (9.03%), Space: 10.8 MB (51.31%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    int houserob(vector<int>& arr){
-        int prev = arr[0];
-        int prev2 = 0;
+    int fun(int i, vector<int>& arr,vector<int>&dp) {
+        if (i >= arr.size())
+            return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take = arr[i] + fun(i + 2, arr,dp);
+        int notTake = fun(i + 1, arr,dp);
 
-        for(int i = 1; i < arr.size(); i++){
-            int take = arr[i];
-            if(i > 1) take += prev2;
-
-            int nottake = prev;
-            int curr = max(nottake, take);
-
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        return dp[i]=max(take, notTake);
     }
 
     int rob(vector<int>& nums) {
-        int n = nums.size();
 
-        if(n == 1) return nums[0]; 
+        if (nums.size() == 1)
+            return nums[0];
 
-        vector<int> robfirst(n-1);
-        vector<int> roblast(n-1);
+        vector<int> temp1;
+        vector<int> temp2;
+        vector<int>dp1(nums.size(),-1);
+        vector<int>dp2(nums.size(),-1);
+        for (int i = 1; i < nums.size(); i++)
+            temp1.push_back(nums[i]);
 
-        for(int i = 0; i < n-1; i++){
-            robfirst[i] = nums[i];
-            roblast[i] = nums[i+1];
-        }
+        for (int i = 0; i < nums.size() - 1; i++)
+            temp2.push_back(nums[i]);
 
-        int rob1 = houserob(robfirst);
-        int rob2 = houserob(roblast);
-
-        return max(rob1, rob2);
+        return max(fun(0, temp1,dp1), fun(0, temp2,dp2));
     }
 };
