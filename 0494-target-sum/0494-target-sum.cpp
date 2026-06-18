ac4461b997102246cc9c7f7564b810d0e351444b
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-06-19 00:36:14 +0530

    Time: 695 ms (19.82%), Space: 11.7 MB (75.7%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     int n;
-
-    int fun(int i, int target, vector<int>& nums, vector<vector<int>>& dp) {
-        if (i == n) {
-            return target == 0;
+    int tar;
+    int count=0;
+    void fun(int i,int total,vector<int>&nums){
+        if(i==n){
+            if(total==tar) count++;
+            return;
         }
-
-        if (dp[i][target] != -1) return dp[i][target];
-
-     
-        if (nums[i] == 0) {
-            return dp[i][target] = 2 * fun(i + 1, target, nums, dp);
-        }
-
-        int take = 0;
-        if (target >= nums[i]) {
-            take = fun(i + 1, target - nums[i], nums, dp);
-        }
-
-        int nottake = fun(i + 1, target, nums, dp);
-
-        return dp[i][target] = take + nottake;
+        fun(i+1,total+nums[i],nums);
+        fun(i+1,total-nums[i],nums);
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
         n = nums.size();
-
-        int totalsum = 0;
-        for (int x : nums) totalsum += x;
-
-       
-        if ((totalsum - target) < 0 || (totalsum - target) % 2 != 0)
-            return 0;
-
-        target = (totalsum - target) / 2;
-
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-
-        return fun(0, target, nums, dp);
+        tar=target;
+        fun(0,0,nums);
+        return count;
     }
 };
