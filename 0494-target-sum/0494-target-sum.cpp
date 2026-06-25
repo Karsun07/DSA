104d95c9e9c400f266d5ce5d9a8cb0dda48d9b4f
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-06-25 11:36:52 +0530

    Time: 854 ms (10.91%), Space: 11.8 MB (76.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int n;
-    int tar;
-    int count=0;
-    void fun(int i,int total,vector<int>&nums){
-        if(i==n){
-            if(total==tar) count++;
-            return;
-        }
-        fun(i+1,total+nums[i],nums);
-        fun(i+1,total-nums[i],nums);
+
+    int fun(int i, int sum, vector<int>& nums, int target) {
+        if (i == n)
+            return sum == target;
+
+        return fun(i + 1, sum + nums[i], nums, target) +
+               fun(i + 1, sum - nums[i], nums, target);
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
         n = nums.size();
-        tar=target;
-        fun(0,0,nums);
-        return count;
+        return fun(0, 0, nums, target);
     }
 };
