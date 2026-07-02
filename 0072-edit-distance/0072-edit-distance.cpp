612faf8421b09a11c0bf18ff8c4270d1ef21e8c7
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-07-02 18:12:10 +0530

    Time: 6 ms (68.7%), Space: 14.2 MB (18.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int n1,n2;
+    int fun(int i,int j,string &word1,string &word2,vector<vector<int>>&dp){
+        if(i==n1){
+            return n2-j;
+        }
+        if(j==n2){
+            return n1-i;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(word1[i]==word2[j]){
+            return dp[i][j]=fun(i+1,j+1,word1,word2,dp);
+        }
+        int ins=1+fun(i,j+1,word1,word2,dp);
+        int del=1+fun(i+1,j,word1,word2,dp);
+        int rep=1+fun(i+1,j+1,word1,word2,dp);
+        return dp[i][j]=min({ins,del,rep});
+    }
     int minDistance(string word1, string word2) {
         n1=word1.size(),n2=word2.size();
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
-        // return fun(n1-1,n2-1,word1,word2,dp);
-        for(int j=1;j<=n2;j++) dp[0][j]=j;
-        for(int i=1;i<=n1;i++) dp[i][0]=i;
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    int ins=1+dp[i][j-1];
-                    int del=1+dp[i-1][j];
-                    int rep=1+dp[i-1][j-1];
-                    dp[i][j]=min({ins,del,rep});
-                }
-            }
-        }
-        return dp[n1][n2];
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return fun(0,0,word1,word2,dp);
 
 
     }
