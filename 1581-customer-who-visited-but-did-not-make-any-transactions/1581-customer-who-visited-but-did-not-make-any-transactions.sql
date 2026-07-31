846cc7df576d670aa0f846ce04c4b32f6975b051
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-07-31 16:31:41 +0530

    Time: 1571 ms (28.12%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,9 @@
+SELECT 
+    v.customer_id,
+    COUNT(v.customer_id) AS count_no_trans
+FROM Visits v
+WHERE v.visit_id NOT IN (
+    SELECT visit_id
+    FROM Transactions
+)
+GROUP BY v.customer_id;
