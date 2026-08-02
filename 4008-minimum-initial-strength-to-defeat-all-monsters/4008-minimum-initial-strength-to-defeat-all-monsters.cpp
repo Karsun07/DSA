e176ede179cf64aa396407522ecece4065d72eab
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-02 11:33:38 +0530

    Time: 17 ms (40%), Space: 243.4 MB (40%) - LeetHub

--- 4008-minimum-initial-strength-to-defeat-all-monsters/4008-minimum-initial-strength-to-defeat-all-monsters.cpp
+++ 4008-minimum-initial-strength-to-defeat-all-monsters/4008-minimum-initial-strength-to-defeat-all-monsters.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    long long minInitialStrength(vector<int>& v, vector<vector<int>>& arr) {
-
-        int n = v.size();
-
-        vector<long long> a(n + 1);
-
-        for(auto &x : arr){
-            int l = x[0], r = x[1], value = x[2];
-
-            a[l] += value;
-            a[r + 1] -= value;
+    long long minInitialStrength(vector<int>& monsters, vector<vector<int>>& boosts) {
+        int n=monsters.size();
+        vector<long long>pref(n,0);
+        for(auto &it:boosts){
+            int i=it[0],j=it[1],b=it[2];
+            pref[i]+=b;
+            if(j+1<n) pref[j+1]-=b;
         }
-
-        for(int i = 1;i <= n;i += 1){
-            a[i] += a[i - 1];
+        for(int i=1;i<n;i++){
+            pref[i]+=pref[i-1];
         }
-
-        long long ans = 0, done = 0;
-
-        for(int i = 0;i < n;i += 1){
-            // a[i] + st >= v[i]
-
-            if(a[i] < v[i]){
-                ans = max(ans, v[i] - a[i] + done);
+        long long prevEnergy=0,ans=0;
+        for(int i=0;i<n;i++){
+            if(pref[i]<monsters[i]){
+                ans=max(ans,monsters[i]-pref[i]+prevEnergy);
             }
-
-            done += v[i];
+            prevEnergy+=monsters[i];
         }
-
         return ans;
-        
+
     }
 };
