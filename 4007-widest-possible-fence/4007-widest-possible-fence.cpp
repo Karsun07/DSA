efe3f26fe343dc3eb5a3f2082efb972316ec655f
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-02 13:03:15 +0530

    Time: 1790 ms (88.89%), Space: 364.5 MB (88.89%) - LeetHub

--- 4007-widest-possible-fence/4007-widest-possible-fence.cpp
+++ 4007-widest-possible-fence/4007-widest-possible-fence.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int maximumWidth(vector<int>& v) {
-        map<int, int> mp;
-
-        int n = v.size();
-
-        for(auto &x : v) mp[x] += 1;
-
-        vector<pair<int, int>> a(mp.begin(), mp.end());
-        // element,freq
-        auto fr = mp;
-
-        for(int i = 0;i < a.size();i += 1){
-
-            auto [x, fx] = a[i];
-            
-            for(int j = i + 1;j < a.size();j += 1){
-                auto [y, fy] = a[j];
-
-                mp[x + y] += min(fx, fy);
+        map<int,int>mp;
+        for(int i=0;i<v.size();i++){
+            mp[v[i]]++;
+        }
+        vector<pair<int,int>>arr(mp.begin(),mp.end());
+        for(int i=0;i<arr.size()-1;i++){
+            auto [e1,f1]=arr[i];
+            for(int j=i+1;j<arr.size();j++){
+                auto [e2,f2]=arr[j];
+                mp[e1+e2]+=min(f1,f2);
             }
-
-            mp[2 * x] += fx/2;
+        mp[e1*2]+=f1/2;
         }
-
-        int ans = 0;
-        for(auto &[x, y] : mp){
-            ans = max(ans, y);
+        int ans=1;
+        for(auto &it:mp){
+            ans=max(ans,it.second);
         }
-
         return ans;
+
+
     }
 };
