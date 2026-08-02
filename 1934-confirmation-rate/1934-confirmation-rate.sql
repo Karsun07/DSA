e9cdeef04ad1b72fb4e6d167af9c4c8cedd77243
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-02 22:39:14 +0530

    Time: 997 ms (6.7%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,8 @@
+SELECT 
+    s.user_id,round(
+        coalesce(sum(c.action='confirmed')/count(c.action),0)
+        ,2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
