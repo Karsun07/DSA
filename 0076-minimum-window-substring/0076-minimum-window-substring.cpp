05c437824695993d4713a56cf528ff268a8094be
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-18 00:56:15 +0530

    Time: 15 ms (35.47%), Space: 11.6 MB (43.48%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int total = t.size();
-        unordered_map<char,int> mp;
 
-        // build from t, not s
-        for(char c : t){
-            mp[c]++;
-        }
+        int n = s.size();
+
+        unordered_map<char,int> mp1;
+        for(int i = 0; i < t.size(); i++)
+            mp1[t[i]]++;
+
+        unordered_map<char,int> mp2;
 
         int l = 0;
-        int start = -1;
-        int size = INT_MAX;
+        int r = 0;
 
-        for(int r = 0; r < s.size(); r++){
-            mp[s[r]]--;
-            if(mp[s[r]] >= 0) total--;
+        int count = 0;  
+        int start = 0;
+        int len = INT_MAX;
 
-            while(total == 0){
-                if(size > r - l + 1){
-                    size = r - l + 1;
+        while(r < n) {
+
+            if(mp1.count(s[r])) {
+                mp2[s[r]]++;
+
+                if(mp2[s[r]] <= mp1[s[r]])
+                    count++;
+            }
+            while(count == t.size()) {
+
+                if(r-l+1 < len) {
+                    len = r-l+1;
                     start = l;
                 }
-
-                mp[s[l]]++;
-                if(mp[s[l]] > 0) total++;
+                if(mp1.count(s[l])) {
+                    mp2[s[l]]--;
+                    if(mp2[s[l]] < mp1[s[l]])
+                        count--;
+                }
                 l++;
             }
+            r++;
         }
 
-        if(start == -1) return "";
-        return s.substr(start, size);
+        return len == INT_MAX ? "" : s.substr(start,len);
     }
 };
