99d3efcd74bae994c5f3a61b1995d0791c1dcae1
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-19 00:32:30 +0530

    Time: 75 ms (5.03%), Space: 77.5 MB (5.03%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int fun(int i,int size,vector<int>&values,vector<vector<int>>&dp){
+        if(i==values.size() && size!=2 ){
+            return INT_MIN;
+        }
+        if(size==2){
+            return 0;
+        }
+        if(dp[i][size]!=-1) return dp[i][size];
+
+        int take=INT_MIN;
+        if(size==0){
+            take=i+values[i]+fun(i+1,size+1,values,dp);
+        }
+        else if(size==1){
+            take=-i+values[i]+fun(i+1,size+1,values,dp);
+        }
+        
+        int nottake=fun(i+1,size,values,dp);
+        
+        return dp[i][size]=max(take,nottake);
+    }
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int n=values.size();
+        vector<vector<int>>dp(n+1,vector<int>(3,-1));
+        return fun(0,0,values,dp);
+        
+    }
+};
