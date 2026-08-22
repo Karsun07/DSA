8031aa6033ea657214a9c93864a3a89d5aea0050
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-22 09:35:31 +0530

    Time: 105 ms (65.59%), Space: 128.5 MB (55.79%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,26 +1,39 @@
 class Solution {
-public: 
-    long long ans=INT_MIN;
-    void dfs(int man,long long cost,vector<vector<int>>&adj,vector<int>&informTime){
-        if(adj[man].empty()){
-            ans=max(ans,cost);
-            return;
+public:
+
+    vector<vector<int>> adj;
+    vector<int> dp;
+
+    int dfs(int man, vector<int>& informTime) {
+
+        if (adj[man].empty())
+            return 0;
+
+        if (dp[man] != -1)
+            return dp[man];
+
+        int mx = 0;
+
+        for (int emp : adj[man]) {
+            mx = max(mx, dfs(emp, informTime));
         }
-        for(int &emp:adj[man]){
-            dfs(emp,cost+1LL*informTime[man],adj,informTime);
-        }
+
+        return dp[man] = mx + informTime[man];
     }
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<vector<int>>adj(n);
-        int man=-1;
+
+    int numOfMinutes(int n, int headID,
+                     vector<int>& manager,
+                     vector<int>& informTime) {
+
+        adj.resize(n);
+        dp.assign(n, -1);
+
         for (int i = 0; i < n; i++) {
             if (manager[i] != -1) {
                 adj[manager[i]].push_back(i);
             }
         }
 
-        dfs(headID, 0, adj, informTime);
-        return ans;
-
+        return dfs(headID, informTime);
     }
 };
