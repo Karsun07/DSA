645efc5738ee25dc2129ce212a4dfd928b633f5a
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-08-25 11:31:24 +0530

    Time: 5 ms (43.8%), Space: 15.8 MB (19.22%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,50 +9,49 @@
  */
 class Solution {
 public:
-    void buildGraph(TreeNode* root, unordered_map<int, vector<int>>& adj) {
-        if (!root) return;
-
-        if (root->left) {
+    void buildGraph(TreeNode* root,unordered_map<int,vector<int>>&adj){
+        if(root->left){
             adj[root->val].push_back(root->left->val);
             adj[root->left->val].push_back(root->val);
-            buildGraph(root->left, adj);
+            buildGraph(root->left,adj);
         }
-        if (root->right) {
-            adj[root->val].push_back(root->right->val);
+        if(root->right){
             adj[root->right->val].push_back(root->val);
-            buildGraph(root->right, adj);
+            adj[root->val].push_back(root->right->val);
+            buildGraph(root->right,adj);
         }
+
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<int, vector<int>> adj;
-        buildGraph(root, adj);
+        unordered_map<int,vector<int>>adj;
 
-        queue<pair<int,int>> q;
-        unordered_set<int> visited;
+        buildGraph(root,adj);
 
-        q.push({target->val, 0});
+        queue<pair<int,int>>q;
+        unordered_set<int>visited;
+
+        q.push({target->val,0});
         visited.insert(target->val);
-
-        vector<int> ans;
-
-        while (!q.empty()) {
-            auto [node, dist] = q.front();
+        vector<int>ans;
+        ans.clear();
+        while(!q.empty()){
+            auto [node,dist]=q.front();
             q.pop();
-
-            if (dist == k) {
+            if(dist==k){
                 ans.push_back(node);
                 continue;
             }
 
-            for (int j=0;j<adj[node].size();j++) {
-                int nb=adj[node][j];
-                if (!visited.count(nb)) {
+            for(auto nb:adj[node]){
+                if(!visited.count(nb)){
                     visited.insert(nb);
-                    q.push({nb, dist + 1});
-                }
+                    q.push({nb,dist+1});
+                }                
             }
         }
-
         return ans;
+
+
+        
     }
 };
