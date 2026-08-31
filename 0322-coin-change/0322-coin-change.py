c209d7bd37479587fd49a71ab14fffcbefdd375e
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-01 02:26:13 +0530

    Time: 955 ms (16.24%), Space: 23.3 MB (23.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,30 +1,27 @@
 class Solution:
-    def fun(self, i, amt, coins, dp):
-
-        if amt == 0:
-            return 0
-
-        if i == len(coins) or amt < 0:
-            return float('inf')
-
-        if dp[i][amt] != -1:
-            return dp[i][amt]
-
-        take = 1 + self.fun(i, amt - coins[i], coins, dp)
-
-        nottake = self.fun(i + 1, amt, coins, dp)
-
-        dp[i][amt] = min(take, nottake)
-
-        return dp[i][amt]
-
     def coinChange(self, coins: List[int], amount: int) -> int:
 
         n = len(coins)
-      
-        dp = [[-1] * (amount + 1) for _ in range(n)]
 
-        ans = self.fun(0, amount, coins, dp)
+        dp = [[float('inf')] * (amount + 1) for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            dp[i][0] = 0
+
+        for i in range(n - 1, -1, -1):
+
+            for amt in range(1, amount + 1):
+
+                nottake = dp[i + 1][amt]
+
+                take = float('inf')
+
+                if coins[i] <= amt:
+                    take = 1 + dp[i][amt - coins[i]]
+
+                dp[i][amt] = min(take, nottake)
+
+        ans = dp[0][amount]
 
         if ans == float('inf'):
             return -1
