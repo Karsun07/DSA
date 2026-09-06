b93ca6270a97a55ac27d2ce3728d550361c1e68b
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-06 19:04:31 +0530

    Time: 375 ms (66.92%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,23 +1,25 @@
-# Write your MySQL query statement below
+SELECT 
+    c.visited_on,
 
-SELECT
-    visited_on,
     (
-        SELECT SUM(amount)
-        FROM customer
-        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+        SELECT SUM(c2.amount)
+        FROM Customer c2
+        WHERE c2.visited_on BETWEEN
+              DATE_SUB(c.visited_on, INTERVAL 6 DAY)
+              AND c.visited_on
     ) AS amount,
-    ROUND(
-        (
-            SELECT SUM(amount) / 7
-            FROM customer
-            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-        ),
-        2
-    ) AS average_amount
-FROM customer c
-WHERE visited_on >= (
-        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
-        FROM customer
-    )
-GROUP BY visited_on;
+
+    ROUND((
+        SELECT SUM(c2.amount) / 7
+        FROM Customer c2
+        WHERE c2.visited_on BETWEEN
+              DATE_SUB(c.visited_on, INTERVAL 6 DAY)
+              AND c.visited_on
+    ), 2) AS average_amount
+
+FROM Customer c
+WHERE c.visited_on >= (
+    SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+    FROM Customer
+)
+GROUP BY c.visited_on;
