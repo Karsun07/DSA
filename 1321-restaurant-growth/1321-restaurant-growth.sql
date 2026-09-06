30fe932712d264cd249677af93609bb4bf5039f7
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-06 19:06:33 +0530

    Time: 728 ms (5%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,25 +1,9 @@
-SELECT 
-    c.visited_on,
-
-    (
-        SELECT SUM(c2.amount)
-        FROM Customer c2
-        WHERE c2.visited_on BETWEEN
-              DATE_SUB(c.visited_on, INTERVAL 6 DAY)
-              AND c.visited_on
-    ) AS amount,
-
-    ROUND((
-        SELECT SUM(c2.amount) / 7
-        FROM Customer c2
-        WHERE c2.visited_on BETWEEN
-              DATE_SUB(c.visited_on, INTERVAL 6 DAY)
-              AND c.visited_on
-    ), 2) AS average_amount
-
-FROM Customer c
-WHERE c.visited_on >= (
-    SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
-    FROM Customer
-)
-GROUP BY c.visited_on;
+select distinct visited_on,
+        sum(amount) over w as amount,
+        round((sum(amount) over w)/7, 2) as average_amount
+    from customer
+    WINDOW w AS ( 
+            order by visited_on
+            range between interval 6 day PRECEDING and current row
+    )
+    Limit 6, 999
