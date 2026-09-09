2fd0268baec1182b5f17efeddbbc71197b73787c
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-09 23:53:52 +0530

    Time: 11 ms (51.91%), Space: 23 MB (29.34%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,36 +5,29 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        pq = []
-        count = 0
+        pq=[]
+        count=0
 
-        # Put first node of every list into heap
-        for node in lists:
-            if node:
-                heapq.heappush(pq, (node.val, count, node))
-                count += 1
-
-        # No nodes
+        for list in lists:
+            if list:
+                heapq.heappush(pq,(list.val,count,list))
+                count+=1
+        
         if not pq:
             return None
 
-        # Dummy node
-        dummy = ListNode(0)
-        tail = dummy
+        dummy=ListNode(0)
+        tail=dummy
 
         while pq:
+            _,_,node=heapq.heappop(pq)
 
-            # Get smallest node
-            _, _, node = heapq.heappop(pq)
+            tail.next=node
+            tail=tail.next
 
-            # Attach it to answer
-            tail.next = node
-            tail = tail.next
-
-            # Put next node of same list into heap
             if node.next:
-                heapq.heappush(pq, (node.next.val, count, node.next))
-                count += 1
+                heapq.heappush(pq,(node.next.val,count,node.next))
+                count+=1
 
         return dummy.next
 
