ae0a1d529dc157d2623f015be5b0b44da01a4218
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-14 21:17:31 +0530

    Time: 1559 ms (42.45%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,23 +1,26 @@
-(
-    SELECT u.name AS results
-    FROM Users u
-    JOIN MovieRating m
-        ON u.user_id = m.user_id
-    GROUP BY u.user_id, u.name
-    ORDER BY COUNT(*) DESC, u.name
-    LIMIT 1
-)
+(select u.name as results
+from MovieRating mr
+join Users u
+on mr.user_id=u.user_id
+group by mr.user_id
+order by count(mr.rating) desc, u.name
+limit 1)
 
-UNION ALL
 
-(
-    SELECT mov.title AS results
-    FROM Movies mov
-    JOIN MovieRating m
-        ON mov.movie_id = m.movie_id
-    WHERE m.created_at >= '2020-02-01'
-      AND m.created_at < '2020-03-01'
-    GROUP BY mov.movie_id, mov.title
-    ORDER BY AVG(m.rating) DESC, mov.title
-    LIMIT 1
-);
+union all
+
+(SELECT m.title AS results
+FROM MovieRating mr
+JOIN Movies m
+    ON mr.movie_id = m.movie_id
+WHERE LEFT(mr.created_at, 7) = '2020-02'
+GROUP BY mr.movie_id, m.title
+ORDER BY AVG(mr.rating) DESC, m.title ASC
+LIMIT 1)
+
+
+
+
+
+
+
