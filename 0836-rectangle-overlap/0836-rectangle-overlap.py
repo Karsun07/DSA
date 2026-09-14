5e70c66f531234041ae39cf91f5721932c4e0455
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-14 12:46:11 +0530

    Time: 0 ms (100%), Space: 19.6 MB (19.53%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.py
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -4,31 +4,22 @@ class Solution:
         r1x1, r1y1, r1x2, r1y2 = rec1
         r2x1, r2y1, r2x2, r2y2 = rec2
 
-        if r1x1 < r2x1:
-            # rec1  on  left
-            if r1x2 <= r2x1:
-                return False
+        # No overlap if:
+        # 1. rec1 is completely to the left of rec2
+        # 2. rec1 is completely to the right of rec2
+        # 3. rec1 is completely above rec2
+        # 4. rec1 is completely below rec2
 
-            if r1y1 >= r2y2:
-                return False
+        if r1x2 <= r2x1:
+            return False
 
-            if r2y1 >= r1y2:
-                return False
+        if r2x2 <= r1x1:
+            return False
 
-        elif r1x1 > r2x1:
-            # rec1  on  right
-            if r1x1 >= r2x2:
-                return False
+        if r1y2 <= r2y1:
+            return False
 
-            if r2y1 >= r1y2:
-                return False
-
-            if r1y1 >= r2y2:
-                return False
-
-        else:
-            # Same x1
-            if r1y1 >= r2y2 or r2y1 >= r1y2:
-                return False
+        if r2y2 <= r1y1:
+            return False
 
         return True
