9064b0e37dbb6a25d92df45a388b18677f949e7a
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-14 19:11:00 +0530

    Time: 345 ms (22.75%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,7 @@
-SELECT
-    CASE
-        WHEN (SELECT COUNT(*) FROM Employee) = 1 THEN NULL
-        ELSE MAX(CASE WHEN t.rnk = 2 THEN t.salary END)
-    END AS SecondHighestSalary
+SELECT MAX(salary) AS SecondHighestSalary
 FROM (
-    SELECT
-        salary,
-        DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    SELECT salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
     FROM Employee
-) t;
+) t
+WHERE t.rnk = 2;
