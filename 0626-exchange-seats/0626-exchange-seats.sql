6707dcbee1e36d4a301c67c6f454d237760bf38b
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-09-14 21:32:54 +0530

    Time: 391 ms (49.27%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,10 @@
 select 
-case
+case 
    when id=(select max(id) from Seat) and id%2=1 then id
    when id%2=0 then id-1
-   else
-       id+1
-end as id, student
-from Seat
-order by id
+   when id%2=1 then id+1
+end as id
+   ,
+   student
+   from Seat
+   order by id
